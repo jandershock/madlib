cf674ec42994826685cfa014a1cc5a7b7636ff49
jandershock <jordan.andershock@gmail.com>
2022-03-29 16:38:39 -0500

    Added some local storage

--- main.js
+++ main.js
@@ -8,27 +8,32 @@ const mainElement = document.querySelector("main");
 mainElement.addEventListener("click", event => {
 	if(event.target.id.startsWith("tellStory")){
 		const madlib = {
-			adjective:"",
-			sillyword:"",
-			pluralnoun:"",
-			noun:""
+			adjective: document.querySelector("#adjective").value,
+			sillyword: document.querySelector("#sillyWord").value,
+			pluralnoun: document.querySelector("#pluralNoun").value,
+			noun: document.querySelector("#noun").value
 		}
 		//set/save to sessionStorage
+		setDataToStorage(madlib);
 		
+		console.log(getDataFromStorage("madlib"));
 		//invoke renderStory
 	}
 })
 
 const getDataFromStorage = (dataKey) => {
 	//use JSON.parse()
+	return JSON.parse(localStorage.getItem(dataKey))
 }
 
 const setDataToStorage = (dataObj) => {
 	//use JSON.stringify()
+	localStorage.setItem("madlib", JSON.stringify(dataObj));
 }
 
 const clearStorage = (dataKey) => {
 	sessionStorage.removeItem(dataKey);
+	localStorage.removeItem(dataKey);
 }
 
 const renderInputs = () => {
